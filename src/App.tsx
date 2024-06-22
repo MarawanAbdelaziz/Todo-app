1ff74009aed1eca0afc78b135fcaad9869cb89eb
MarawanAbdelaziz <marawan.abdelaziz33@gmail.com>
2024-06-22 23:23:52 +0300

    ا

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import ReduxTodo from "./components/ReduxTodo";
+import Todo from "./components/Todo";
+import ZustandTodo from "./components/ZustandTodo";
+
+function App() {
+  return (
+    <>
+      <Todo />;
+      <ReduxTodo />
+      <ZustandTodo />
+    </>
+  );
+}
+
+export default App;
